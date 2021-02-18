26db1b3b58ef7447c46fe0cca12553677ed457d0
Peyton Jones <peytonj.dev@gmail.com>
2021-02-18 10:28:18 -0700

    add styling and class names to main page

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./MainPage.scss";
-import alienHead from "../../assets/alien-head.jpg";
+import alienHead from "../../assets/alien-outline.png";
 import { Link } from "react-router-dom";
 
 const MainPage = () => {
   return (
-    <main>
-      <div>
-        <img src={alienHead} alt="alien head" />
-        <div>
+    <main className="main">
+      <div className="main-content">
+        <img src={alienHead} alt="alien head" className="alien-logo"/>
+        <div className="btns">
           <Link to="/sightings-map">
             <button>Sightings</button>
           </Link>
