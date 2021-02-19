910881f2a126d306bb2dad5f9d75ea78f0033e76
mdflynn <mdflynn34@outlook.com>
2021-02-18 19:07:19 -0700

    Add Map to page

--- src/Components/SightingsMap/SightingsMap.js
+++ src/Components/SightingsMap/SightingsMap.js
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+};
+
+const center = {
+  lat: 33.394,
+  lng: -104.525
+};
 
 const SightingsMap = () => {
-  return (
-    <main>
-      <h1>Map here</h1>
-    </main>
-  )
-}
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: `${process.env.REACT_APP_API_KEY}`,
+  });
+
+  const [map, setMap] = useState(null);
+  const [selectedCenter, setSelectedCenter] = useState(null);
+
+  const onLoad = useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
+};
 
 export default SightingsMap;
