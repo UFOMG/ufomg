8185a8406487907b46b5555c3f7c32b66ab30327
mdflynn <mdflynn34@outlook.com>
2021-02-21 11:57:27 -0700

    clean up

--- src/mockdata.js
+++ src/mockdata.js
@@ -1,53 +1,55 @@
 export const mockSightings = [
   {
-    name: '',
-    lat: '47.7511',
-    lng: '-120.7401',
-    description: 'BIG BLUE BALL IN SKY OMG',
-    eventType: 'sighting',
-    image: 'https://cdn.mos.cms.futurecdn.net/QkuLCPzH7GUVS9MthdWM9M.jpg',
+    name: "",
+    lat: "47.7511",
+    lng: "-120.7401",
+    description: "BIG BLUE BALL IN SKY OMG",
+    eventType: "sighting",
+    image: "https://cdn.mos.cms.futurecdn.net/QkuLCPzH7GUVS9MthdWM9M.jpg",
   },
   {
-    name: 'Billy Bob',
-    lat: '43.8041',
-    lng: '-120.5542',
-    description: 'BIG RED BALL IN SKY OMG',
-    eventType: 'encounter',
-    image: 'https://ewscripps.brightspotcdn.com/dims4/default/3402aa2/2147483647/strip/true/crop/1000x563+0+0/resize/1280x720!/quality/90/?url=http%3A%2F%2Fewscripps-brightspot.s3.amazonaws.com%2F5a%2F57%2F58a3662b422a8fb6464451f244eb%2Flarge-fireball-seen-across-fl-sky-mari-g.png',
+    name: "Billy Bob",
+    lat: "43.8041",
+    lng: "-120.5542",
+    description: "BIG RED BALL IN SKY OMG",
+    eventType: "encounter",
+    image:
+      "https://ewscripps.brightspotcdn.com/dims4/default/3402aa2/2147483647/strip/true/crop/1000x563+0+0/resize/1280x720!/quality/90/?url=http%3A%2F%2Fewscripps-brightspot.s3.amazonaws.com%2F5a%2F57%2F58a3662b422a8fb6464451f244eb%2Flarge-fireball-seen-across-fl-sky-mari-g.png",
   },
   {
-    name: 'Stephanie Jo',
-    lat: '44.0682',
-    lng: '-114.7420',
-    description: 'rotating lights',
-    eventType: 'abduction',
-    image: 'https://i.dailymail.co.uk/i/pix/2016/10/04/01/391305FA00000578-3820675-image-a-23_1475542666609.jpg',
+    name: "Stephanie Jo",
+    lat: "44.0682",
+    lng: "-114.7420",
+    description: "rotating lights",
+    eventType: "abduction",
+    image:
+      "https://i.dailymail.co.uk/i/pix/2016/10/04/01/391305FA00000578-3820675-image-a-23_1475542666609.jpg",
   },
   {
-    name: 'Mike',
-    lat: '39.5501',
-    lng: '-105.7821',
-    description: 'lights and probes everywhere',
-    eventType: 'abduction',
-    image: 'https://www.thebalance.com/thmb/AnCvkiXlyT69MPJMJYtOcNyLSsw=/2121x1413/filters:fill(auto,1)/AlienAbductionInsurancePolicyWhatYouNeedtoKnow-5a9c31e1c6733500374d6d7a.jpg',
+    name: "Mike",
+    lat: "39.5501",
+    lng: "-105.7821",
+    description: "lights and probes everywhere",
+    eventType: "abduction",
+    image:
+      "https://www.thebalance.com/thmb/AnCvkiXlyT69MPJMJYtOcNyLSsw=/2121x1413/filters:fill(auto,1)/AlienAbductionInsurancePolicyWhatYouNeedtoKnow-5a9c31e1c6733500374d6d7a.jpg",
   },
   {
-    name: 'Fox Mulder',
-    lat: '41.8780',
-    lng: '-93.0977',
-    description: 'lights everywhere',
-    eventType: 'lights',
-    image: 'https://www.thebalance.com/thmb/AnCvkiXlyT69MPJMJYtOcNyLSsw=/2121x1413/filters:fill(auto,1)/AlienAbductionInsurancePolicyWhatYouNeedtoKnow-5a9c31e1c6733500374d6d7a.jpg',
+    name: "Fox Mulder",
+    lat: "41.8780",
+    lng: "-93.0977",
+    description: "lights everywhere",
+    eventType: "lights",
+    image:
+      "https://www.thebalance.com/thmb/AnCvkiXlyT69MPJMJYtOcNyLSsw=/2121x1413/filters:fill(auto,1)/AlienAbductionInsurancePolicyWhatYouNeedtoKnow-5a9c31e1c6733500374d6d7a.jpg",
   },
   {
-    name: 'Dana Scully',
-    lat: '31.9686',
-    lng: '-99.9018',
-    description: 'flying saucer with lil green men',
-    eventType: 'encounter',
-    image: 'https://www.thebalance.com/thmb/AnCvkiXlyT69MPJMJYtOcNyLSsw=/2121x1413/filters:fill(auto,1)/AlienAbductionInsurancePolicyWhatYouNeedtoKnow-5a9c31e1c6733500374d6d7a.jpg',
-  }
-
-]
-
-// lat / long converted to numbers?
+    name: "Dana Scully",
+    lat: "31.9686",
+    lng: "-99.9018",
+    description: "flying saucer with lil green men",
+    eventType: "encounter",
+    image:
+      "https://www.thebalance.com/thmb/AnCvkiXlyT69MPJMJYtOcNyLSsw=/2121x1413/filters:fill(auto,1)/AlienAbductionInsurancePolicyWhatYouNeedtoKnow-5a9c31e1c6733500374d6d7a.jpg",
+  },
+];
