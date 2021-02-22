7dbdb388d10abe7541bcd91c4d3d36e3ca06fbab
mdflynn <mdflynn34@outlook.com>
2021-02-22 15:04:57 -0700

    add fetch call to be dev repo

--- src/api.js
+++ src/api.js
@@ -1,11 +1,25 @@
-export const apiCall = () => {
-  return fetch("http://localhost:5000/api/v1/reports")
-    .then((response) => {
-      if (response.status >= 200 && response.status <= 299) {
-        return response.json();
-      } else {
-        console.log(`Error! Code: ${response}`);
-        return response.status;
-      }
-    })
+import {
+  fetchSightingsPending,
+  fetchSightingsSuccess,
+  fetchSightingsError,
+} from "./actions";
+
+const fetchSightings = () => {
+  return (dispatch) => {
+    dispatch(fetchSightingsPending());
+    fetch("http://localhost:5000/api/v1/reports")
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.error) {
+          throw response.error;
+        }
+        dispatch(fetchSightingsSuccess(response.results));
+        return response.results;
+      })
+      .catch((error) => {
+        dispatch(fetchSightingsError(error));
+      });
+  };
 };
+
+export default fetchSightings;
