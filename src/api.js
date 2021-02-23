b382c938750f3512eb803984c042a43202e787d5
mdflynn <mdflynn34@outlook.com>
2021-02-23 13:15:31 -0700

    add post request

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ import {
   fetchSightingsPending,
   fetchSightingsSuccess,
   fetchSightingsError,
+  postSightingsSuccess,
 } from "./actions";
 
 const fetchSightings = () => {
@@ -20,6 +21,23 @@ const fetchSightings = () => {
         dispatch(fetchSightingsError(error));
       });
   };
+};
+
+export const postSighting = (sighting, dispatch) => {
+  fetch("http://localhost:5000/api/v1/reports", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(sighting),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.error) {
+        throw response.error;
+      }
+      dispatch(postSightingsSuccess(sighting));
+    });
 };
 
 export default fetchSightings;
