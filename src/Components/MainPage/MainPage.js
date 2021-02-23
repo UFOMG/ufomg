5853b4539e71dc1667ec8e24113f25dd510815f2
mdflynn <mdflynn34@outlook.com>
2021-02-23 14:27:13 -0700

    refactor for hooks

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -2,19 +2,14 @@ import React, { useEffect } from "react";
 import "./MainPage.scss";
 import alienHead from "../../assets/alien-head.jpg";
 import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import fetchSightingsAction from "../../api";
-import {
-  getSightings,
-  getSightingsError,
-  getSightingsPending,
-} from "../../reducers/sightings";
+import fetchSightings from "../../api";
+import { useDispatch } from "react-redux";
 
-const MainPage = (props) => {
+const MainPage = () => {
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    const { fetchSightings } = props;
-    fetchSightings();
+    fetchSightings(dispatch);
   });
 
   return (
@@ -22,10 +17,12 @@ const MainPage = (props) => {
       <div className="main-content">
         <div id="app">
           <div id="wrapper">
-            <h1 className="glitch" data-text="UFOMG">UFOMG</h1>
+            <h1 className="glitch" data-text="UFOMG">
+              UFOMG
+            </h1>
           </div>
         </div>
-        <img src={alienHead} alt="alien head" className="alien-logo"/>
+        <img src={alienHead} alt="alien head" className="alien-logo" />
         <div className="btns">
           <Link to="/sightings-map">
             <button className="main-btns">Sightings</button>
@@ -39,18 +36,4 @@ const MainPage = (props) => {
   );
 };
 
-const mapStateToProps = (state) => ({
-  error: getSightingsError(state),
-  sightings: getSightings(state),
-  pending: getSightingsPending(state),
-});
-
-const mapDispatchToProps = (dispatch) =>
-  bindActionCreators(
-    {
-      fetchSightings: fetchSightingsAction,
-    },
-    dispatch
-  );
-
-export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
+export default MainPage;
