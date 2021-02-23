584b4bd23048eb9e311db90d12509c7ba3f69ac8
Jordon Smith <jordon00smith@gmail.com>
2021-02-23 15:19:36 -0700

    Add test for viewing sightings page

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -5,4 +5,11 @@ describe('App', () => {
     cy.get('[href="/sightings-map"] > .main-btns').should('contain', 'Sightings')
     cy.get('[href="/report-form"] > .main-btns').should('contain', 'Report Sighting')
   })
+
+  it('should allow users to view sightings', () => {
+    cy.visit('http://localhost:3000/')
+
+    cy.get('[href="/sightings-map"] > .main-btns').click()
+    cy.url().should('include', '/sightings-map')
+  })
 })
