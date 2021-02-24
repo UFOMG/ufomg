e658e98db2bbe76c6abaed9503284946d18cfc89
mdflynn <mdflynn34@outlook.com>
2021-02-23 20:00:31 -0700

    reverse address api

--- src/api.js
+++ src/api.js
@@ -6,19 +6,19 @@ import {
 } from "./actions";
 
 const fetchSightings = (dispatch) => {
-    dispatch(fetchSightingsPending());
-    fetch("http://localhost:5000/api/v1/reports")
-      .then((response) => response.json())
-      .then((response) => {
-        if (response.error) {
-          throw response.error;
-        }
-        dispatch(fetchSightingsSuccess(response.results));
-        return response.results;
-      })
-      .catch((error) => {
-        dispatch(fetchSightingsError(error));
-      });
+  dispatch(fetchSightingsPending());
+  fetch("http://localhost:5000/api/v1/reports")
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.error) {
+        throw response.error;
+      }
+      dispatch(fetchSightingsSuccess(response.results));
+      return response.results;
+    })
+    .catch((error) => {
+      dispatch(fetchSightingsError(error));
+    });
 };
 
 export const postSighting = (sighting, dispatch) => {
@@ -36,6 +36,14 @@ export const postSighting = (sighting, dispatch) => {
       }
       dispatch(postSightingsSuccess(sighting));
     });
+};
+
+export const reverseAddress = (city, state) => {
+  const formatCity = city.split(" ").join("+");
+  const formatState = state.split(" ").join("+");
+  return fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${formatCity},+${formatState}&key=${process.env.REACT_APP_API_KEY}`
+  ).then((response) => response.json());
 };
 
 export default fetchSightings;
