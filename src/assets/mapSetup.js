3c21a8a81fb86abb923719a48704ea2fcd088f36
mdflynn <mdflynn34@outlook.com>
2021-02-25 15:42:34 -0700

    add gradients

--- src/assets/mapSetup.js
+++ src/assets/mapSetup.js
@@ -8,7 +8,22 @@ export const mapCenter = {
   lng: -98.5795,
 };
 
-
+export const mapGradient = [
+  "rgba(0, 255, 255, 0)", // less dense
+  "rgba(0, 255, 255, 1)",
+  "rgba(0, 191, 255, 1)",
+  "rgba(0, 127, 255, 1)",
+  "rgba(0, 63, 255, 1)",
+  "rgba(0, 0, 255, 1)",
+  "rgba(0, 0, 223, 1)",
+  "rgba(0, 0, 191, 1)",
+  "rgba(0, 0, 159, 1)",
+  "rgba(0, 0, 127, 1)",
+  "rgba(63, 0, 91, 1)",
+  "rgba(127, 0, 63, 1)",
+  "rgba(191, 0, 31, 1)",
+  "rgba(255, 0, 0, 1)" // more dense
+];
 
 export const customMap = [
   {
