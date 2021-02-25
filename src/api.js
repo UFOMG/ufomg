8b576c0ed7fd1cdf0add05e378c1fbef611a1f3e
mdflynn <mdflynn34@outlook.com>
2021-02-24 18:23:54 -0700

    add reverseGeolocation

--- src/api.js
+++ src/api.js
@@ -38,12 +38,20 @@ export const postSighting = (sighting, dispatch) => {
     });
 };
 
-export const reverseAddress = (city, state) => {
+export const geolocateUser = (city, state) => {
   const formatCity = city.split(" ").join("+");
   const formatState = state.split(" ").join("+");
+  // check for CO vs Colorado
   return fetch(
     `https://maps.googleapis.com/maps/api/geocode/json?address=${formatCity},+${formatState}&key=${process.env.REACT_APP_API_KEY}`
   ).then((response) => response.json());
 };
+
+// if locate me functionality
+export const reverseGeolocateUser = (lat, long) => {
+  return fetch (
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${process.env.REACT_APP_API_KEY}`
+  )
+}
 
 export default fetchSightings;
