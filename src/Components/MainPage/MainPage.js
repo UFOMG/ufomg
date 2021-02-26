f858e3d9ebbc747edeec393f406e704eecaa21c5
Peyton Jones <peytonj.dev@gmail.com>
2021-02-26 10:40:39 -0700

    add ufo animation and styling

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./MainPage.scss";
-import alienHead from "../../assets/alien-outline.png";
 import { Link } from "react-router-dom";
 import fetchSightings from "../../api";
 import { useDispatch } from "react-redux";
+
 
 const MainPage = () => {
   const dispatch = useDispatch();
@@ -12,25 +12,194 @@ const MainPage = () => {
     fetchSightings(dispatch);
   });
 
+
+
   return (
     <main className="main">
-      <div className="main-content">
-        <div id="app">
-          <div id="wrapper">
-            <h1 className="glitch" data-text="UFOMG">
-              UFOMG
-            </h1>
+      <div className="main-content ">
+        <section className='content'>
+          <div class="scene">
+          <div class="ufoWrap">
+          <div class='center'>
+            <div class="ufoInner">
+              <div id='ufo'>
+                <ul class='blinkers'>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <div class="dome"></div>
+                <div class="antigrav">
+                  <div class="rings">
+                    <div class="inner"></div>
+                    <ul>
+                      <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="thrust">
+                  <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-        <img src={alienHead} alt="alien head" className="alien-logo" />
-        <div className="btns">
-          <Link to="/sightings-map">
-            <button className="main-btns">Sightings</button>
-          </Link>
-          <Link to="/report-form">
-            <button className="main-btns">Report Sighting</button>
-          </Link>
-        </div>
+          </div>
+          <div class="foreground">
+            <ul class="trees3">
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+            <ul class="trees2">
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+            <ul class="trees1">
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+          <div class="background">
+            <div class="stars">
+              <li class="small"></li>
+              <li class="small"></li>
+              <li></li>
+              <li class="small"></li>
+              <li class="small"></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li class="small"></li>
+              <li></li>
+              <li></li>
+              <li class="small"></li>
+              <li class="small"></li>
+              <li class="small"></li>
+              <li class="small"></li>
+              <li></li>
+              <li class="small"></li>
+              <li class="small"></li>
+              <li class="small"></li>
+              <li></li>
+              <li class="small"></li>
+              <li class="small"></li>
+              <li></li>
+              <li class="small"></li>
+              <li class="small"></li>
+              <li class="small"></li>
+              <li class="small"></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li class="small"></li>
+              <li></li>
+              <li class="small"></li>
+              <li class="small"></li>
+              <li></li>
+              <li></li>
+              <li class="small"></li>
+              <li></li>
+              <li class="small"></li>
+              <li class="small"></li>
+              <li></li>
+              <li class="small"></li>
+              <li class="small"></li>
+              <li class="small"></li>
+              <li class="small"></li>
+              <li></li>
+              <li></li>    
+            </div>
+          </div>
+          </div>
+          <div class="camera">
+              <div class="battery">
+                <div class="juice"></div>
+              </div>
+              <div class="rec">
+                <span><span></span>REC</span>
+              </div>
+              <div class="meta">
+                <p>F2.3&nbsp;&nbsp;0dB&nbsp;&nbsp;15.7V 
+                  <span class="exposure">
+                    <span class="plus">+</span>
+                    <span class="minus">-</span>
+                  </span>
+                </p>
+              </div>
+              <div class="timer">
+                <label id="hours">00</label>:
+                <label id="minutes">00</label>:
+                <label id="seconds">00</label>
+              </div>
+          </div>
+          <div id="app">
+            <div id="wrapper">
+              <h1 className="glitch" data-text="UFOMG">
+                UFOMG
+              </h1>
+            </div>
+          </div>
+          <div className="main-btns">
+            <Link to="/sightings-map">
+              <button className="btns">Sightings</button>
+            </Link>
+            <Link to="/report-form">
+              <button className="btns">Report Sighting</button>
+            </Link>
+          </div>
+        </section>
       </div>
     </main>
   );
