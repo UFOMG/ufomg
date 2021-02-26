5b515a6007e62a5fbede44cac1c1c19a9753c3fe
mdflynn <mdflynn34@outlook.com>
2021-02-26 11:44:13 -0700

    add route for sightings filter

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import MainPage from "../MainPage/MainPage";
 import ReportForm from "../ReportForm/ReportForm";
 import MapDisplay from "../MapDisplay/MapDisplay";
+import SightingsFilter from "../SightingsFilter/SightingsFilter";
 import { Route, Switch } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" component={MainPage} />
         <Route exact path="/report-form" component={ReportForm} />
         <Route exact path="/sightings-map" component={MapDisplay} />
+        <Route exact path="/sighting-reports" component={SightingsFilter} />
       </Switch>
     </>
   );
