2cab381ed54db2755f57e146f3ec19880e62057d
mdflynn <mdflynn34@outlook.com>
2021-02-28 20:01:38 -0700

    add comment action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,10 @@
-// export const addSighting = (eventDetails) => ({
-//   type: "ADD_SIGHTING",
-//   eventDetails,
-// });
-
 //  GET data
 export const FETCH_SIGHTINGS_PENDING = "FETCH_SIGHTINGS_PENDING";
 export const FETCH_SIGHTINGS_SUCCESS = "FETCH_SIGHTINGS_SUCCESS";
 export const FETCH_SIGHTINGS_ERROR = "FETCH_SIGHTINGS_ERROR";
+export const FETCH_COMMENTS_SUCCESS = "FETCH_COMMENTS_SUCCESS";
 //  POST data
-// export const POST_SIGHTINGS_PENDING = "POST_SIGHTINGS_PENDING";
 export const POST_SIGHTINGS_SUCCESS = "POST_SIGHTINGS_SUCCESS";
-// export const POST_SIGHTINGS_ERROR = "POST_SIGHTINGS_ERROR";
 
 export const fetchSightingsPending = () => {
   return {
@@ -36,5 +30,12 @@ export const postSightingsSuccess = (sighting) => {
   return {
     type: POST_SIGHTINGS_SUCCESS,
     sighting,
+  };
+};
+
+export const fetchCommentsSuccess = (sighting) => {
+  return {
+    type: FETCH_COMMENTS_SUCCESS,
+    singleSighting: sighting,
   };
 };
