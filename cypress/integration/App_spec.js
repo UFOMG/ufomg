d19cab7021662ca655dc7650e6a2d4182de07117
Jordon Smith <jordon00smith@gmail.com>
2021-03-02 16:58:52 -0700

    Add tests for viewing a sighting

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -1,16 +1,31 @@
 describe('App', () => {
-  it('should render a home page', () => {
+  it('should render a home page with three buttons', () => {
     cy.visit('http://localhost:3000/')
 
-    cy.get('[href="/sightings-map"] > .main-btns').should('contain', 'Sightings')
-    cy.get('[href="/report-form"] > .main-btns').should('contain', 'Report Sighting')
+    cy.get('[href="/sightings-map"] > .btns').should('contain', 'Sightings')
+    cy.get('[href="/report-form"] > .btns').should('contain', 'Report Sighting')
+    cy.get('[href="/sighting-research"] > .btns').should('contain', 'Research')
   })
 
-  it('should allow users to view sightings', () => {
+  it('should allow users to view sightings page', () => {
     cy.visit('http://localhost:3000/')
 
-    cy.get('[href="/sightings-map"] > .main-btns').click()
+    cy.get('[href="/sightings-map"] > .btns').click()
     cy.url().should('include', '/sightings-map')
+
+    cy.get('.button-div > :nth-child(1)').should('contain', 'Toggle HeatMap')
+    cy.get('.button-div > :nth-child(2)').should('contain', 'Show Recent Activity')
+    cy.get('[style="z-index: 3; position: absolute; height: 100%; width: 100%; padding: 0px; border-width: 0px; margin: 0px; left: 0px; top: 0px; touch-action: pan-x pan-y;"]')
+  })
+
+  it.only('should allow users to view a sightings details', () => {
+    cy.visit('http://localhost:3000/')
+
+    cy.get('[href="/sightings-map"] > .btns').click()
+    cy.url().should('include', '/sightings-map')
+
+    cy.get('[style="position: absolute; left: 0px; top: 0px; z-index: 106; width: 100%;"] > :nth-child(57) > img').click()
+    cy.get('.gm-style-iw-d > :nth-child(1) > :nth-child(1) > div > :nth-child(2)').should('contain', 'it was chill')
   })
 
   it('should allow users to fill out a sighting form and submit', () => {
