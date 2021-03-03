60813ee9154c2d172e847e790451e0b354b1023d
mdflynn <mdflynn34@outlook.com>
2021-03-02 17:04:03 -0700

    move function to global

--- src/Components/SightingsFilter/SightingsFilter.js
+++ src/Components/SightingsFilter/SightingsFilter.js
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./SightingsFilter.scss";
 import { Link } from "react-router-dom";
-import { usStates } from "../../utilities/mapSetup";
+import { usStates, generateStockImage } from "../../utilities/mapSetup";
 import { useSelector } from "react-redux";
-import alienstock from "../../assets/alienstock.jpeg";
-import lightsstock from "../../assets/lightsstock.jpeg";
-import ufostock from "../../assets/ufostock.jpeg";
 
 const SightingsFilter = () => {
   const [selectedState, setSelectedState] = useState("");
@@ -27,19 +24,6 @@ const SightingsFilter = () => {
     return sightings.sightings.filter((sighting) => {
       return sighting.state.toLowerCase() === state.toLowerCase();
     });
-  };
-
-  const generateStockImage = (eventType) => {
-    switch (eventType) {
-      case "sighting":
-        return lightsstock;
-      case "encounter":
-        return alienstock;
-      case "abduction":
-        return ufostock;
-      default:
-        return lightsstock;
-    }
   };
 
   const getFilteredComments = (filteredSightings) => {
